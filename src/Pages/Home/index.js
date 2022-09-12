9934ab7446696a23f1ada47f28b78b980a913b95
Jaiveer yadav <dev.jaiveer.yadav@gmail.com>
2022-09-12 18:24:51 +0530

    setup

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+
+function Home() {
+  return (
+       <div className='Home'>
+            <Navbar />
+    </div>
+  )
+}
+
+export default Home
