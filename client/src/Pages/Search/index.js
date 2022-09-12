fddbb878b460efbfb434864953337d25dd1bd58a
Jaiveer yadav <dev.jaiveer.yadav@gmail.com>
2022-09-12 21:55:47 +0530

    search half done

--- client/src/Pages/Search/index.js
+++ client/src/Pages/Search/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Search() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState();
+  const apikey = "4624eb9a";
+  const response = () => {
+    axios
+      .get(`https://www.omdbapi.com/?s=${search}&apikey=${apikey}`)
+      .then((response) => {
+        setData(response);
+        console.log(data)
+      });
+
+  }
+
+
   return (
-    <div>Search</div>
-  )
+    <div className="Search">
+      <form>
+        <input
+          type="text"
+          placeholder="Movie"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <input type="button" value="submit" onClick={response} />
+      </form>
+      <div>
+        
+      </div>
+    </div>
+  );
 }
 
-export default Search
+export default Search;
